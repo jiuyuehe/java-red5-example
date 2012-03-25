ad717a0733fe84bbc3589f4b51df65a222485f16
Kengo Nakatsuka <kengo.nakatsuka@gmail.com>
2012-03-25 16:35:52 +0900

    Initial commit.

---
+++ src/jp/nak2k/red5/example/Application.java
@@ -0,0 +1,16 @@
+package jp.nak2k.red5.example;
+
+import org.red5.server.adapter.ApplicationAdapter;
+
+/**
+ * 
+ * @author Kengo Nakatsuka
+ *
+ */
+public class Application extends ApplicationAdapter {
+	/**
+	 * 
+	 */
+	public Application() {
+	}
+}
